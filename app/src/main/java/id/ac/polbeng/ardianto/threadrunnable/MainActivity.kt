28fa3cd89b8940165d9ea200fc6fce67eaf090a5
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-30 04:56:25 +0700

    Update Commit2

--- app/src/main/java/id/ac/polbeng/ardianto/threadrunnable/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/ardianto/threadrunnable/MainActivity.kt
@@ -1,10 +1,7 @@
 package id.ac.polbeng.ardianto.threadrunnable
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import id.ac.polbeng.ardianto.threadrunnable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
